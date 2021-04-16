9ae1812598c84b6b4efd64733c2f0a71a0171cc8
PedroOliveiraSimoes <pedroppsimoes@gmail.com>
2021-04-15 21:49:49 -0300

    struct do grafo, adaptacao a entrada

--- Guloso/Caixeiro.cpp
+++ Guloso/Caixeiro.cpp
@@ -1,2 +1,25 @@
 #include <iostream>
-//pronto fiz minha parte
+#include <vector>
+#include <cmath>
+struct Vertice {
+    int x;
+    int y;
+    Vertice() = default;
+    Vertice(int x, int y);
+    double distance(Vertice V) {
+        return std::sqrt((std::pow(this->x - V.x, 2) + std::pow(this->y - V.y, 2)));
+    }
+};
+Vertice::Vertice(int x, int y) : x(x), y(y) {}
+int main() {
+    int N, X, Y;
+    std::cin >> N;
+    std::vector<Vertice> grafo(N);
+    for (int i = 0; i < N; i++) {
+        std::cin >> X;
+        std::cin >> Y;
+        grafo.push_back(Vertice(X, Y));
+    }
+
+    return 0;
+}
