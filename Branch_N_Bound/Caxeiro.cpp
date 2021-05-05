909b489f8afce162f70e4678f084ee260ddc7f8d
PedroOliveiraSimoes <pedroppsimoes@gmail.com>
2021-05-05 08:10:31 -0300

    comentei os trem

--- Branch_N_Bound/Caxeiro.cpp
+++ Branch_N_Bound/Caxeiro.cpp
@@ -4,8 +4,20 @@
 #include <cmath>
 #include <limits>
 #define infinity std::numeric_limits<double>::infinity()
-int N, idNodes;
-std::vector<double> matrizRaiz;
+/*
+ALGORITMO BRANCH AND BOUND PARA O PROBLEMA DO CAXEIRO VIAJANTE
+André Lucas Ribeiro Costa - 666400
+Pedro Oliveira Simões - 654068
+Complexidade:
+*/
+
+/*
+    Estrutura de Nodo para realizar a logica branch and bound
+    Cada nodo representa um passo no caminho do caxeiro a ser descoberto
+    Nodos contem um vetor de nodos filhos, uma matriz da adjacencia, um vetor que 
+        indica o caminho percorrido no grafo para chegar neste nodo e o custo do caminho ate
+        o proprio
+*/
 struct Node {
     int id, idVertice;
     std::vector<Node*> filhos;
@@ -26,6 +38,12 @@ Node::Node(int Id, int IdV, std::vector<int>Caminho, Node* Pai) : id(Id), idVert
     this->pai = Pai;
     this->caminho = Caminho;
 }
+/*
+    Estrutura de vertice para criar o grafo a partir de entrada fornecida
+    Guarda o vertice com sua cordenada(x,y)
+    Para este exemplo, serve mais como abstracao, nao foi tao necessario quanto nos outros
+        problemas
+*/
 struct Vertice {
     int x;
     int y;
@@ -37,7 +55,6 @@ struct Vertice {
 };
 Vertice::Vertice(int x, int y) : x(x), y(y) {}
 
-
 bool contains(std::vector<int> caminho, int buscado) {
     int i = 0;
     bool resp = false;
@@ -48,9 +65,14 @@ bool contains(std::vector<int> caminho, int buscado) {
     return resp;
 }
 
+/*
+    Efetua a reducao da matriz de adjacencia fornecida, deixando pelo menos
+    um zero em todas as linhas e colunas da matriz.
+    Colunas e linhas com infinito sao ignoradas
+*/
 double reduceMatrix(std::vector<double>& matriz) {
     double valorReducao = 0, menor = 0;
-    //deixar pelo menos um zero em todas as linhas e colunas da matriz,colunas e linhas com infinito sao ignoradas
+    //loop for para reduzir as linhas
     for (int i = 0; i < N; i++) {
         menor = infinity;
         for (int j = 0; j < N; j++) {
@@ -62,6 +84,7 @@ double reduceMatrix(std::vector<double>& matriz) {
         }
         valorReducao += menor;
     }
+    //loop for para reduzir as colunas
     for (int i = 0; i < N; i++) {
         menor = infinity;
         for (int j = 0; j < N; j++) {
@@ -84,12 +107,18 @@ void searchMinimum(Node* node) {
             menorCustoSearch = node->custo;
             menorSearch = node;
         }
-    }
-    for (int i = 0; i < node->filhos.size(); i++) {
+    }else for (int i = 0; i < node->filhos.size(); i++) {
         searchMinimum(node->filhos[i]);
     }
 }
 
+/*
+    Funcao de expansao de um nodo para encontrar o caminho do caxeiro
+    Checa-se o caminho percorrido ate o nodo a ser expandido para ver quais
+        vertices ainda sao elegiveis para continuar o caminho
+    É criado um filho para cada vertice elegivel
+    Cada filho recebe um custo de acordo com a matriz de adjacencia do pai
+*/
 void expand(Node* node) {
     if (node->caminho.size() < N && node->folha == true) {
         node->folha = false;
@@ -98,13 +127,17 @@ void expand(Node* node) {
                 Node* novo = new Node(idNodes, i, node->caminho, node);
                 novo->caminho.push_back(i);
                 novo->matriz = node->matriz;
-                //se a ligacao e do vertice A para o B, linha A e coluna B recebem infinito
+
+                //Se a ligacao é do vertice A para o B, linha A e coluna B recebem infinito
                 for (int j = 0; j < N; j++) {
                     novo->matriz[(N * node->idVertice) + j] = infinity;
                     novo->matriz[(N * j) + novo->idVertice] = infinity;
                 }
+
+                //ligacao de B para A tambem recebe infinito
                 novo->matriz[(N * novo->idVertice) + node->idVertice] = infinity;
-                //custo do nodo filho = custo do pai + W(Pai,Filho) + custo de reducao da matriz
+
+                //Custo do nodo filho = custo do pai + W(Pai,Filho) + custo de reducao da matriz
                 novo->custo = node->custo + matrizRaiz[(N * node->idVertice) + novo->idVertice] + reduceMatrix(novo->matriz);
                 node->filhos.push_back(novo);
                 idNodes++;
@@ -112,31 +145,33 @@ void expand(Node* node) {
         }
     }
 }
-
+int N, idNodes;
+std::vector<double> matrizRaiz;
 int main() {
     idNodes = 0;
     int X, Y;
     std::cin >> N;
     std::vector<Vertice> grafo;
     std::vector<double> matrizAdj(N * N, infinity);
+    //Criacao do grafo a partir da entrada
     for (int i = 0; i < N; i++) {
         std::cin >> X;
         std::cin >> Y;
         grafo.push_back(Vertice(X, Y));
     }
-
+    //Criacao da matriz de adjacencia do grafo
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             if (i != j) {
                 matrizAdj[(i * N) + j] = grafo[i].distance(grafo[j]);
             }
             else matrizAdj[(i * N) + j] = infinity;
-            //std::cout << matrizAdj[(i * N) + j] << " ";
         }
     }
     double custo = reduceMatrix(matrizAdj);
     matrizRaiz = matrizAdj;
 
+    //Criacao da raiz da arvore com o vertice 0 de incial
     Node* raiz = new Node(idNodes, 0);
     idNodes++;
     raiz->custo = custo;
@@ -144,11 +179,18 @@ int main() {
     raiz->caminho.push_back(0);
 
 
+    
     expand(raiz);
     menorCustoSearch = infinity;
     searchMinimum(raiz);
     bool done = false;
     double menorCaminho = 0;
+    /*
+        Loop While para realizar a expansao da arvore ate encontrar o menor caminho
+        Procura-se o vertice com menor custo(que seja uma folha), expande-se o mesmo, procura-se o menor
+            custo novamente.
+        Quando o vertice de menor custo tiver um caminho completo, o loop acaba.
+    */
     while (!done) {
         if (menorSearch->caminho.size() == N) {
             done = true;
@@ -156,7 +198,7 @@ int main() {
                 menorCaminho += grafo[menorSearch->caminho[i]].distance(grafo[menorSearch->caminho[i + 1]]);
             }
             menorCaminho += grafo[menorSearch->caminho[menorSearch->caminho.size() - 1]].distance(grafo[menorSearch->caminho[0]]);
-            std::cout << std::fixed << std::setprecision(2) << menorCaminho << std::endl;
+            std::cout << std::fixed << std::setprecision(2) << menorCaminho <<", "<< menorSearch->custo << std::endl;
             for (int i = 0; i < menorSearch->caminho.size(); i++) {
                 std::cout << menorSearch->caminho[i] + 1 << " ";
             }
--- Guloso/Caixeiro.cpp
+++ Guloso/Caixeiro.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
+/*
+ALGORITMO GULOSO PARA O PROBLEMA DO CAXEIRO VIAJANTE
+André Lucas Ribeiro Costa - 666400
+Pedro Oliveira Simões - 654068
+Complexidade: 
+*/
 struct Vertice {
     int x;
     int y;
@@ -17,26 +23,42 @@ int main() {
     int N, X, Y;
     std::cin >> N;
     std::vector<Vertice> grafo;
+    /*
+    Leitura da entrada e criacao do grafo
+    */
     for (int i = 0; i < N; i++) {
         std::cin >> X;
         std::cin >> Y;
         grafo.push_back(Vertice(X, Y));
     }
+
     std::vector<int> refs,menorCaminho;
     double soma = 0, menor = 1000000000;
+    /*
+    Como o caminho do caxeiro comeca sempre no vertice '0', 
+        cria-se um vetor 'refs' com os vertices restantes
+        possiveis para o caminho(1 ate N)
+    */
     for (int i = 1; i < N; i++) {
         refs.push_back(i);
     }
+    /*
+    Para cada permutacao do caminho, 
+    calcular o valor do ciclo e atualizar o valor do menor caminho encontrado
+    */
     do {
         soma = 0;
-        soma += grafo[0].distance(grafo[refs[0]]);
-        for (int i = 0; i < N-2; i++) soma += grafo[refs[i]].distance(grafo[refs[i+1]]);
-        soma += grafo[refs[refs.size()-1]].distance(grafo[0]);
+        soma += grafo[0].distance(grafo[refs[0]]); // OPERACAO RELEVANTE
+        for (int i = 0; i < N - 2; i++) {
+            soma += grafo[refs[i]].distance(grafo[refs[i + 1]]); // OPERACAO RELEVANTE
+        }
+        soma += grafo[refs[refs.size()-1]].distance(grafo[0]); // OPERACAO RELEVANTE
         if (soma < menor) {
             menor = soma;
             menorCaminho = refs;
         }
     } while (std::next_permutation(refs.begin(),refs.end()));
+
     std::cout << std::fixed << std::setprecision(2) << menor << std::endl;
     std::cout << "1 ";
     for (int i = 0; i < N-1; i++) {
